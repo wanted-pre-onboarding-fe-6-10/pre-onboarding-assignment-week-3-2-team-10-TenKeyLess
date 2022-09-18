ff407e4a75b571dae8c1b6bac55b582ef0d115ff
cnxk <cos4338@gmail.com>
2022-09-18 22:28:46 +0900

    Feat: create Comment Box with map method

--- src/pages/commponents/InputBox.tsx
+++ src/pages/commponents/InputBox.tsx
@@ -1,18 +1,70 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
-import useInput from '../../hoc/useInput';
-import { Comments } from '../../types/types';
+import { postCommentApi } from '../../api/api';
+import { INITIAL_STATE, postComment } from '../../redux/modules/comment';
+import { rootReducer } from '../../redux/store';
 
 const InputBox = () => {
-  const [{ profile, author, content, createAt }, onChange, reset] = useInput({
-    profile_url: '',
+  const input = useSelector((store: ReturnType<typeof rootReducer>) => {
+    return store.comment.comment;
   });
+
+  const dispatch = useDispatch();
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(
+      postComment({
+        ...input,
+        [e.target.name]: e.target.value,
+      })
+    );
+  };
+
+  const addComment: React.MouseEventHandler<HTMLButtonElement> = async (input: any) => {
+    await postCommentApi(input);
+  };
 
   return (
     <Container>
-      <Input type="text" placeholder="profile-img" />
-      <Input type="text" placeholder="작성자" />
-      <TextArea placeholder="내용" />
-      <Input type="text" placeholder="xxxx-xx-xx" />
+      <Input
+        type="text"
+        placeholder="프로필"
+        value={input.profile_url}
+        name="profile_url"
+        onChange={onChange}
+      />
+      <Input
+        type="text"
+        placeholder="작성자"
+        name="author"
+        value={input.author}
+        onChange={onChange}
+      />
+      <Input
+        type="textarea"
+        placeholder="내용"
+        name="content"
+        value={input.content}
+        onChange={onChange}
+      />
+      <Input
+        type="text"
+        placeholder="작성일"
+        name="createdAt"
+        value={input.createdAt}
+        onChange={onChange}
+      />
+      <Button
+        type="submit"
+        onClick={e => {
+          e.preventDefault();
+          addComment(input);
+          dispatch(postComment(INITIAL_STATE.comment));
+        }}
+      >
+        등록하기
+      </Button>
     </Container>
   );
 };
@@ -28,8 +80,7 @@ const Input = styled.input.attrs(props => ({
   type: props.type,
   placeholer: props.placeholder,
 }))``;
-const TextArea = styled.textarea.attrs(props => ({
-  placeholer: props.placeholder,
-}))`
-  resize: none;
+
+const Button = styled.button`
+  height: 30px;
 `;
