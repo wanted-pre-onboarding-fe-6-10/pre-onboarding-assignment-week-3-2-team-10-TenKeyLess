3f2c6198d8d16d68ceda8ee38370765a95929385
cnxk <cos4338@gmail.com>
2022-09-19 01:08:31 +0900

    Feat: add redux action (move_page, edit_mode)

--- src/redux/modules/comment.ts
+++ src/redux/modules/comment.ts
@@ -1,10 +1,15 @@
+import { createExpressionWithTypeArguments } from 'typescript';
 import { Comments } from '../../types/types';
 
 // Action Type
 
 export const GET_COMMENTS = 'comment/GET_COMMENTS' as const;
+export const ADD_COMMENTS = 'comment/ADD_COMMENTS' as const;
 export const POST_COMMENTS = 'comment/POST_COMMENTS' as const;
 export const DELETE_COMMENTS = 'comment/DELETE_COMMENTS' as const;
+export const UPDATE_COMMENTS = 'comment/UPDATE_COMMENTS' as const;
+export const EDIT_MODE = 'comment/EDIT_MODE' as const;
+export const MOVE_PAGE = 'comment/MOVE_PAGE' as const;
 
 // // Action Creator
 
@@ -12,7 +17,11 @@ export const getComment = (comment: Array<Comments>) => {
   return { type: GET_COMMENTS, payload: comment };
 };
 
-export const postComment = (comment: any) => {
+export const addNewComment = (comment: Comments) => {
+  return { type: ADD_COMMENTS, payload: comment };
+};
+
+export const postComment = (comment: Comments) => {
   return { type: POST_COMMENTS, payload: comment };
 };
 
@@ -20,9 +29,26 @@ export const deleteComment = (id: number) => {
   return { type: DELETE_COMMENTS, id };
 };
 
+export const updateComment = (id: number, comment: Comments) => {
+  return { type: UPDATE_COMMENTS, payload: comment, id };
+};
+
+export const editMode = (mode: boolean) => {
+  return { type: EDIT_MODE, mode };
+};
+
+export const movePage = (page: string) => {
+  return { type: MOVE_PAGE, page };
+};
+
 // Initail State
 
-export const INITIAL_STATE: { commentList: Array<Comments>; comment: Comments } = {
+export const INITIAL_STATE: {
+  commentList: Array<Comments>;
+  comment: Comments;
+  page: string;
+  editMode: boolean;
+} = {
   commentList: [],
   comment: {
     id: 0,
@@ -31,6 +57,8 @@ export const INITIAL_STATE: { commentList: Array<Comments>; comment: Comments } 
     profile_url: '',
     createdAt: '',
   },
+  page: '1',
+  editMode: false,
 };
 
 // Reducer
@@ -39,13 +67,32 @@ const commentReducer = (state = INITIAL_STATE, action: any) => {
   switch (action.type) {
     case GET_COMMENTS:
       return { ...state, commentList: action.payload };
+
+    case ADD_COMMENTS:
+      const addedList = [...state.commentList, action.payload];
+      return { ...state, commentList: addedList };
+
     case POST_COMMENTS:
       return { ...state, comment: action.payload };
+
     case DELETE_COMMENTS:
       const newList = state.commentList.filter(comment => {
         return comment.id !== action.id;
       });
       return { ...state, commentList: newList };
+
+    case UPDATE_COMMENTS:
+      const updatedList = state.commentList.map(comment => {
+        return comment.id === action.id ? action.payload : comment;
+      });
+      return { ...state, commentList: updatedList };
+
+    case EDIT_MODE:
+      return { ...state, editMode: action.mode };
+
+    case MOVE_PAGE:
+      return { ...state, page: action.page };
+
     default:
       return state;
   }
