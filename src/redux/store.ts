dcbc0e1d37c386e5b2101976574f71581cae82d4
cnxk <cos4338@gmail.com>
2022-09-19 02:26:02 +0900

    Feat: Add redux dev-toolkit and logger

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,10 @@
 import { composeWithDevTools } from '@redux-devtools/extension';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
 import commentReducer from './modules/comment';
 
 export const rootReducer = combineReducers({ comment: commentReducer });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 
 export default store;
