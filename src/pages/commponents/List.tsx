e843fb89d8437d9fd6e000e90c16e1e9b2ed6aa0
cnxk <cos4338@gmail.com>
2022-09-18 22:27:33 +0900

    Feat: API get method, create Comment List

--- src/pages/commponents/List.tsx
+++ src/pages/commponents/List.tsx
@@ -1,18 +1,70 @@
 import { useEffect } from 'react';
-import JasonServerApi from '../../api/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteCommentApi, getCommentApi } from '../../api/api';
+import { deleteComment, getComment } from '../../redux/modules/comment';
+import { rootReducer } from '../../redux/store';
+import { Comments } from '../../types/types';
+import styled from 'styled-components';
 
 const List = () => {
-  /* api 호출 */
-  // const getItems = new JasonServerApi();
-  // const getList = async () => {
-  //   const res = await getItems.getComments();
-  // };
+  // get List
+  const list = useSelector((store: ReturnType<typeof rootReducer>) => {
+    return store.comment.commentList;
+  });
 
-  // useEffect(() => {
-  //   getList();
-  // }, []);
+  const dispatch = useDispatch();
 
-  return <div>list</div>;
+  const getList = async () => {
+    const response = await getCommentApi();
+    dispatch(getComment(response));
+  };
+
+  const removeComment = async (id: number) => {
+    const response = await deleteCommentApi(id);
+    dispatch(deleteComment(id));
+  };
+
+  useEffect(() => {
+    getList();
+  }, [list]);
+
+  return (
+    <Container>
+      {list.map((comment: Comments) => {
+        return (
+          <CommentBox key={comment.id}>
+            <ProfileWrapper>
+              <Profile src={comment.profile_url} />
+              <Author>{comment.author}</Author>
+            </ProfileWrapper>
+            <Content>{comment.content}</Content>
+            <ButtonWrapper>
+              <Button>수정</Button>
+              <Button
+                onClick={() => {
+                  removeComment(comment.id);
+                }}
+              >
+                삭제
+              </Button>
+            </ButtonWrapper>
+          </CommentBox>
+        );
+      })}
+    </Container>
+  );
 };
 
 export default List;
+
+const Container = styled.ul``;
+const CommentBox = styled.li`
+  display: flex;
+  flex-direction: column;
+`;
+const ProfileWrapper = styled.div``;
+const Profile = styled.img``;
+const Author = styled.span``;
+const Content = styled.p``;
+const ButtonWrapper = styled.div``;
+const Button = styled.button``;
